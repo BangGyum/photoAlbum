a94859a2ee0a2077375a2eb8bb2b55f8d0a95c42
LXV <bgpro714@naver.com>
2023-03-03 16:08:48 +0900

    useEffect async error resolve

--- src/main/frontend/src/Routes/InAlbum.js
+++ src/main/frontend/src/Routes/InAlbum.js
@@ -11,24 +11,31 @@ function InAlbum() {
     //앨범 목록 가져오기
   const [albumListName, setAlbumListName] = useState([]);
   const [albumListId, setAlbumListId] = useState([]);
-  useEffect(async ()=>{
-    const album = await axios.get("/albums/albumList?sort=byName");
-    console.log(album);
+  useEffect(()=>{
+      async function fetchAlbums() {
+            try {
+                const album = await axios.get("/albums/albumList?sort=byName");
+                console.log(album);
 
-    for(let i=0; i < album.data.length; i++){
-        console.log(album.data[i]);
-        setAlbumListName((albumListName) => {
-              return [...albumListName, album.data[i].AlbumName];
-            });
-        setAlbumListId((albumListId) => {
-                      return [...albumListId, album.data[i].albumId];
-                    });
-        //setThumbUrl([...thumbUrl,movies.data[i].thumbUrl], () => console.log(this.thumbUrl));
+                for(let i=0; i < album.data.length; i++){
+                    console.log(album.data[i]);
+                    setAlbumListName((albumListName) => {
+                          return [...albumListName, album.data[i].AlbumName];
+                        });
+                    setAlbumListId((albumListId) => {
+                                  return [...albumListId, album.data[i].albumId];
+                                });
+                    //setThumbUrl([...thumbUrl,movies.data[i].thumbUrl], () => console.log(this.thumbUrl));
 
-        }
-    console.log(albumListName);
-
+                    }
+            console.log(albumListName);
+            } catch (error) {
+                console.error(error);
+      }
+    }
+    fetchAlbums();
   },[]);
+
 
     const navigate = useNavigate(); //양식이 제출 or 특정 event가 발생 시, url을 조작할 수 있는 interface
     const onClickLogOut = () => {
@@ -43,7 +50,7 @@ return (
         <button onClick={onClickLogOut}>Log out</button>
         <ul>
         {albumListId.map((albumId, index) => (
-            <li><Link to="/photoAlbum">InPhotoAlbum</Link></li>
+            <li><Link to="/photoAlbum" key={index}>InPhotoAlbum</Link></li>
         ))}
         </ul>
     </>
