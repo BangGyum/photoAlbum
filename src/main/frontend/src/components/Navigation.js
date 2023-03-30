b37e921c1dd144dfe86b73e3c18329c5a7065c08
LXV <bgpro714@naver.com>
2023-03-30 22:56:44 +0900

    getEachAlbumPhotos app success22

--- src/main/frontend/src/components/Navigation.js
+++ src/main/frontend/src/components/Navigation.js
@@ -13,17 +13,3 @@ const Navigation = () => { //link를 사용해서 이동
 }
 export default Navigation;
 
-import React from "react";
-import { Link } from "react-router-dom";
-
-const Navigation = () => { //link를 사용해서 이동
-    return(
-
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/album">InAlbum</Link></li>
-            <li><Link to="/photoAlbum/:propId">InPhotoAlbum</Link></li>
-        </ul>
-);
-}
-export default Navigation;
