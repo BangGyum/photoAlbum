f7753346e12bb85e17ac7076eb2b1869411f782b
LXV <bgpro714@naver.com>
2023-04-08 17:14:40 +0900

    inalbum check

--- src/main/frontend/src/Routes/InAlbum.js
+++ src/main/frontend/src/Routes/InAlbum.js
@@ -1,65 +1,65 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
-import AppRouter from "components/Router";
-import { authService } from "FirebaseInstance";
-import { useNavigate, Link } from "react-router-dom";
-//import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react';
-//import chakraTheme from '@chakra-ui/theme';
-import InPhotoAlbum from "Routes/InPhotoAlbum";
-import "css/albumStyle.css";
+ import axios from 'axios';
+ import AppRouter from "components/Router";
+ import { authService } from "FirebaseInstance";
+ import { useNavigate, Link } from "react-router-dom";
+ //import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react';
+ //import chakraTheme from '@chakra-ui/theme';
+ import InPhotoAlbum from "Routes/InPhotoAlbum";
+ import "css/albumStyle.css";
 
-//앨범 목록 가져오기
-function InAlbum() {
-  const [albumList, setAlbumList] = useState([]);
-  //const [albumListId, setAlbumListId] = useState([]);
-  useEffect(()=>{
-      async function fetchAlbums() {
-        try {
-            const album = await axios.get("/albums/albumList?sort=byName");
-            for(let i=0; i < album.data.length; i++){
-                setAlbumList((albumList) => {
-                  return [...albumList, {
-                    id : album.data[i].albumId,
-                    name : album.data[i].albumName,
-                    },
-                  ]});
-            }
-        } catch (error) {
-            console.error(error);
-      }
-    }
-    fetchAlbums();
-  },[]);
+ //앨범 목록 가져오기
+ function InAlbum() {
+   const [albumList, setAlbumList] = useState([]);
+   //const [albumListId, setAlbumListId] = useState([]);
+   useEffect(()=>{
+       async function fetchAlbums() {
+         try {
+             const album = await axios.get("/albums/albumList?sort=byName");
+             for(let i=0; i < album.data.length; i++){
+                 setAlbumList((albumList) => {
+                   return [...albumList, {
+                     id : album.data[i].albumId,
+                     name : album.data[i].albumName,
+                     },
+                   ]});
+             }
+         } catch (error) {
+             console.error(error);
+       }
+     }
+     fetchAlbums();
+   },[]);
 
-    const navigate = useNavigate(); //양식이 제출 or 특정 event가 발생 시, url을 조작할 수 있는 interface
-    const onClickLogOut = () => {
-        authService.signOut();
+     const navigate = useNavigate(); //양식이 제출 or 특정 event가 발생 시, url을 조작할 수 있는 interface
+     const onClickLogOut = () => {
+         authService.signOut();
 
-        navigate(-1);  //뒤로
-    }
+         navigate(-1);  //뒤로
+     }
 
-    console.log("aaa",albumList);
-return (
+     console.log("aaa",albumList);
+ return (
 
-        <>
-        <button onClick={onClickLogOut}>Log out</button>
-        <div className="albumFolder1">
-        {albumList.map((albumListEach, index) => (
-            <Link to={`/photoAlbum/${albumListEach.id}`} key={index}>
-                {console.log(albumListEach.id)};
-                <div className="albumFolder2" >
-                    name : {albumListEach.name} , id: {albumListEach.id}
+         <>
+         <button onClick={onClickLogOut}>Log out</button>
+         <div className="albumFolder1">
+         {albumList.map((albumListEach, index) => (
+             <Link to={`/photoAlbum/${albumListEach.id}`} key={index}>
+                 {console.log(albumListEach.id)};
+                 <div className="albumFolder2" >
+                     name : {albumListEach.name} , id: {albumListEach.id}
 
-                </div>
-                <input type="checkbox" onChange={(e) => {
-                    const checked = e.target.checked;  // 체크 여부
-                    console.log(`checkbox ${index} is ${checked ? 'checked' : 'unchecked'}`);
-                }} />
-            </Link>
-        ))}
-        </div>
-    </>
+                 </div>
+                 <input type="checkbox" onChange={(e) => {
+                     const checked = e.target.checked;  // 체크 여부
+                     console.log(`checkbox ${index} is ${checked ? 'checked' : 'unchecked'}`);
+                 }} />
+             </Link>
+         ))}
+         </div>
+     </>
 
-);
-}
-export default InAlbum;
+ );
+ }
+ export default InAlbum;
