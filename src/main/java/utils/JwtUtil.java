2da6c1755a2519cc87ac32925dcb616037a07c5a
LXV <bgpro714@naver.com>
2023-10-12 22:23:11 +0900

    resolve jsonwebtoken

--- src/main/java/utils/JwtUtil.java
+++ src/main/java/utils/JwtUtil.java
@@ -1,5 +1,7 @@
 package utils;
-//import io.jsonwebtoken.*;
+import io.jsonwebtoken.*;
+
+import java.util.Date;
 
 
 public class JwtUtil {
@@ -10,10 +12,19 @@ public class JwtUtil {
      * @param userName  : 사용자 이름
      * @return String : 토큰
      */
-//    public static createJwt(String userName, String secretKey){ //secretKey는 토큰에 서명하는데 사용, userName은
-//                                                            // controller에서 check 할때 토큰의 유저네임을 꺼내서 표시
-//                                                            // jwt는 클레임이라고, 내가 원하는 정보를 저장하는 공간을 제공
-//        Claims claims = Jwts.claims();
-//
-//    }
+    //static이니깐 userService 만들어서 거기서 리턴해주자.
+    public static String createJwt(String userName, String secretKey, Long expiredMs){ //secretKey는 토큰에 서명하는데 사용, userName은
+                                                            // controller에서 check 할때 토큰의 유저네임을 꺼내서 표시
+                                                            // jwt는 클레임이라고, 내가 원하는 정보를 저장하는 공간을 제공
+                                                            // expired밀리세컨드
+        Claims claims = Jwts.claims();
+        claims.put("userName", userName);
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiredMs))        //언제까지
+                .signWith(SignatureAlgorithm.HS256,secretKey) //해당 알고리즘으로 사인되었다.
+                .compact();         //jw
+
+    }
 }
